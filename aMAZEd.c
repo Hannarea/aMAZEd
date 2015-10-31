4352b49ec7b386e23ccc841ed2cfba7cc8aa1ea0
Hanna <hreed3@Knights.ucf.edu>
2015-10-31 19:02:09 -0400

    made functions to check if walls are breakable

--- aMAZEd.c
+++ aMAZEd.c
@@ -1,10 +1,14 @@
 // aMAZEd.c
 
 #include <stdio.h>
-#define SIZE 5
+#define SIZE 10
 
 void initialize_maze(int maze[SIZE][SIZE]);
 void print_maze(int maze[SIZE][SIZE]);
+int is_right_breakable(int maze[SIZE][SIZE], int i, int j);
+int is_left_breakable(int maze[SIZE][SIZE], int i, int j);
+int is_up_breakable(int maze[SIZE][SIZE], int i, int j);
+int is_down_breakable(int maze[SIZE][SIZE], int i, int j);
 
 int main(){
     int maze[SIZE][SIZE];
@@ -37,4 +41,50 @@ void print_maze(int maze[SIZE][SIZE]){
         printf("\n");
     }
 }
+
+int is_right_breakable(int maze[SIZE][SIZE], int i, int j){
+    int  check = 0;
+    if (j == SIZE - 1){
+        check = 0;
+    }
+    else if (maze[i][j] != maze[i][j + 1]){
+        check = 1;
+    }
+    return check;
+}
+
+int is_left_breakable(int maze[SIZE][SIZE], int i, int j){
+    int  check = 0;
+    if (j == 0){
+        check = 0;
+    }
+    else if (maze[i][j] != maze[i][j - 1]){
+        check = 1;
+    }
+    return check;
+}
+
+int is_down_breakable(int maze[SIZE][SIZE], int i, int j){
+    int  check = 0;
+    if (i == SIZE - 1){
+        check = 0;
+    }
+    else if (maze[i][j] != maze[i + 1][j]){
+        check = 1;
+    }
+    return check;
+}
+
+
+int is_up_breakable(int maze[SIZE][SIZE], int i, int j){
+    int  check = 0;
+    if (i == 0 ){
+        check = 0;
+    }
+    else if (maze[i][j] != maze[i - 1][j]){
+        check = 1;
+    }
+    return check;
+}
+
 
