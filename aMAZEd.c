04459df212f4e12c0b570bff3b5a2b588eb6f9a1
Hanna <hreed3@Knights.ucf.edu>
2015-10-31 15:23:31 -0400

    added functions to initialize and print maze

--- aMAZEd.c
+++ aMAZEd.c
@@ -2,10 +2,38 @@
 
 #include <stdio.h>
 
+void initialize_maze(int maze[5][5]);
+void print_maze(int maze[5][5]);
+
 int main(){
-    printf("Hello World...!\n");
+    int maze[5][5];
+    int i, j;
+
+    initialize_maze(maze);
+
+    print_maze(maze);
+
+
     return 0;
 }
 
+void initialize_maze(int maze[5][5]){
+    int i, j;
+    for(i=0; i<5; i++){
+        for (j=0; j<5; j++){
+            maze[i][j] = 5 * i + j;
+        }
+    }
+}
 
+
+void print_maze(int maze[5][5]){
+    int i, j;
+    for(i=0; i<5; i++){
+        for (j=0; j<5; j++){
+            printf("%3d ", maze[i][j]);
+        }
+        printf("\n");
+    }
+}
 
