25a82e6482e5e9038c1e0a05a85e4eff0cef5e2f
Hanna <hreed3@Knights.ucf.edu>
2015-10-31 15:29:32 -0400

    made a constant for the size of the maze for versitility

--- aMAZEd.c
+++ aMAZEd.c
@@ -1,12 +1,13 @@
 // aMAZEd.c
 
 #include <stdio.h>
+#define SIZE 5
 
-void initialize_maze(int maze[5][5]);
-void print_maze(int maze[5][5]);
+void initialize_maze(int maze[SIZE][SIZE]);
+void print_maze(int maze[SIZE][SIZE]);
 
 int main(){
-    int maze[5][5];
+    int maze[SIZE][SIZE];
     int i, j;
 
     initialize_maze(maze);
@@ -17,20 +18,20 @@ int main(){
     return 0;
 }
 
-void initialize_maze(int maze[5][5]){
+void initialize_maze(int maze[SIZE][SIZE]){
     int i, j;
-    for(i=0; i<5; i++){
-        for (j=0; j<5; j++){
-            maze[i][j] = 5 * i + j;
+    for(i=0; i<SIZE; i++){
+        for (j=0; j<SIZE; j++){
+            maze[i][j] = SIZE * i + j;
         }
     }
 }
 
 
-void print_maze(int maze[5][5]){
+void print_maze(int maze[SIZE][SIZE]){
     int i, j;
-    for(i=0; i<5; i++){
-        for (j=0; j<5; j++){
+    for(i=0; i<SIZE; i++){
+        for (j=0; j<SIZE; j++){
             printf("%3d ", maze[i][j]);
         }
         printf("\n");
