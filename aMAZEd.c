239a453b23ec108e36c63410d6e39e5554817672
Hanna <hreed3@Knights.ucf.edu>
2015-11-01 16:26:54 -0500

    created functions to decide if a wall is breakable, break walls, and to check if the maze is complete

--- aMAZEd.c
+++ aMAZEd.c
@@ -11,6 +11,11 @@ int is_up_breakable(int maze[SIZE][SIZE], int i, int j);
 int is_down_breakable(int maze[SIZE][SIZE], int i, int j);
 void initialize_v_walls(int walls[SIZE][SIZE - 1]);
 void initialize_h_walls(int walls[SIZE - 1][SIZE]);
+void break_right_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j);
+void break_left_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j);
+void break_up_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j);
+void break_down_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j);
+int maze_not_complete(int maze[SIZE][SIZE]);
 
 int main(){
     int maze[SIZE][SIZE];
@@ -110,3 +115,56 @@ void initialize_h_walls(int walls[SIZE - 1][SIZE]){
         }
     }
 }
+
+// given the room index, compute the wall index and break
+void break_right_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j){
+    walls[i][j] = 0;
+    replace_number(maze, maze[i][j], maze[i][j + 1]);
+}
+void break_left_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j){
+    walls[i][j - 1] = 0;
+    replace_number(maze, maze[i][j], maze[i][j - 1]);
+}
+void break_up_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j){
+    walls[i - 1][j] = 0;
+    replace_number(maze, maze[i][j], maze[i - 1][j]);
+}
+void break_down_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j){
+    walls[i][j] = 0;
+    replace_number(maze, maze[i][j], maze[i + 1][j]);
+}
+
+void replace_number(int maze[SIZE][SIZE], int a, int b){
+    int max_num, min_num, i, j;
+
+    // predict what the max and min are
+    max_num = a;
+    min_num = b;
+    // check if we were right :) ... but... most likely we were... cause we are the best!
+    if (a < b){
+        max_num = b;
+        min_num = a;
+    }
+    // transverse through the maze and replace the maxs with the mins
+    for (i=0; i<SIZE; i++){
+        for (j=0; j<SIZE; j++){
+            if (maze[i][j] == max_num){
+                maze[i][j] = min_num;
+            }
+        }
+    }
+}
+
+int maze_not_complete(int maze[SIZE][SIZE]){
+    int i, j;
+    for (i=0; i<SIZE; i++){
+        for(j=0; j<SIZE; j++){
+            if (maze[i][j] != 0){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+
