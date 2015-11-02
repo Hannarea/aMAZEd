eeb1a7c1a1be93af7e6dbbf05496e0e168d4a6eb
Hanna <hreed3@Knights.ucf.edu>
2015-11-01 22:21:47 -0500

    updated comments

--- aMAZEd.c
+++ aMAZEd.c
@@ -1,19 +1,67 @@
 // aMAZEd.c
 
 #include <stdio.h>
-#define SIZE 10
+#define SIZE 25
 
 /**
  * Initializes the values in each cell of the maze
  * @param maze: 2D array containing the cell numbers
  */
 void initialize_maze(int maze[SIZE][SIZE]);
+/**
+ * Prints the initialized maze
+ * @param maze: 2D initilized array
+ */
 void print_maze(int maze[SIZE][SIZE]);
+/**
+ * Checks if the right wall of a cell is breakable
+ * @param maze: 2D array containing cell numbers
+ * @param i: row of the cell
+ * @param j: column of the cell
+ * @pre
+ * @post checks if the wall is valid to break
+ * @return 1 if the wall is breakable, 0 if the wall is not breakable
+ */
 int is_right_breakable(int maze[SIZE][SIZE], int i, int j);
+/**
+ * Checks if the left wall of a cell is breakable
+ * @param maze: 2D array containing cell numbers
+ * @param i: row of the cell
+ * @param j: column of the cell
+ * @pre
+ * @post checks if the wall is valid to break
+ * @return 1 if the wall is breakable, 0 if the wall is not breakable
+ */
 int is_left_breakable(int maze[SIZE][SIZE], int i, int j);
+/**
+ * Checks if the top wall of a cell is breakable
+ * @param maze: 2D array containing cell numbers
+ * @param i: row of the cell
+ * @param j: column of the cell
+ * @pre
+ * @post checks if the wall is valid to break
+ * @return 1 if the wall is breakable, 0 if the wall is not breakable
+ */
 int is_up_breakable(int maze[SIZE][SIZE], int i, int j);
+/**
+ * Checks if the bottom wall of a cell is breakable
+ * @param maze: 2D array containing cell numbers
+ * @param i: row of the cell
+ * @param j: column of the cell
+ * @pre
+ * @post checks if the wall is valid to break
+ * @return 1 if the wall is breakable, 0 if the wall is not breakable
+*/
 int is_down_breakable(int maze[SIZE][SIZE], int i, int j);
+/**
+ * Initializes the values of the vertical walls to 1
+ * @param v_walls: 2D array containing a 1 or 0 depending on if a wall exists or not
+ */
 void initialize_v_walls(int walls[SIZE][SIZE - 1]);
+/**
+ * Initializes the values of the horizontal walls to 1
+ * @param h_walls: 2D array containing a 1 or 0 depending on if a wall exists or not
+ */
 void initialize_h_walls(int walls[SIZE - 1][SIZE]);
 /**
  * Breaks the wall on the right of a cell
@@ -25,11 +73,53 @@ void initialize_h_walls(int walls[SIZE - 1][SIZE]);
  * @post the corresponding wall value in walls is 0
  */
 void break_right_wall(int maze[SIZE][SIZE], int walls[SIZE][SIZE - 1], int i, int j);
+/**
+ * Breaks the wall on the left of a cell
+ * @param maze: 2D array containing the cell numbers
+ * @param walls: 2D array containing the vertical walls
+ * @param i: row of the cell
+ * @param j: column of the cell
+ * @pre the wall to break is valid
+ * @post the corresponding wall value in walls is 0
+ */
 void break_left_wall(int maze[SIZE][SIZE], int walls[SIZE][SIZE - 1], int i, int j);
+/**
+ * Breaks the wall on the top of a cell
+ * @param maze: 2D array containing the cell numbers
+ * @param walls: 2D array containing the vertical walls
+ * @param i: row of the cell
+ * @param j: column of the cell
+ * @pre the wall to break is valid
+ * @post the corresponding wall value in walls is 0
+ */
 void break_up_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j);
+/**
+ * Breaks the wall on the top of a cell
+ * @param maze: 2D array containing the cell numbers
+ * @param walls: 2D array containing the vertical walls
+ * @param i: row of the cell
+ * @param j: column of the cell
+ * @pre the wall to break is valid
+ * @post the corresponding wall value in walls is 0
+ */
 void break_down_wall(int maze[SIZE][SIZE], int walls[SIZE - 1][SIZE], int i, int j);
+/**
+ * checks if the maze is complete or not
+ * transverses through the maze array to check if all values are 0
+ * @return returns a 1 if the maze is not complete, returns a 0 if the maze is complete
+ */
 int maze_not_complete(int maze[SIZE][SIZE]);
+/**
+ * replaces the number indicating a section of a maze with the other (creating one section) when a wall is broken
+ * @param maze: a 2D array containing cell numbers
+ * @param a: the value of the cell corresponding to maze[i][j]
+ * @param b: the value of the cell adjacent to maze[i][j]
+ * @pre a and b must bevalues of adjacent cells (either R, L, Up or Down)
+ */
 void replace_number(int maze[SIZE][SIZE], int a, int b);
+/**
+ *
+ */
 void assemble_maze(int maze[SIZE][SIZE], int v_walls[SIZE][SIZE - 1], int h_walls[SIZE - 1][SIZE]);
 void print_walls(int h_walls[SIZE - 1][SIZE],int v_walls[SIZE][SIZE - 1]);
 
@@ -37,16 +127,21 @@ int main(){
     int maze[SIZE][SIZE];
     int v_walls[SIZE][SIZE - 1];
     int h_walls[SIZE - 1][SIZE];
-    int i, j;
 
+    // setting all values of maze to 0 through (SIZE - 1)
     initialize_maze(maze);
+    // setting all the values of v_walls to 1, indicating that there are vertical walls
     initialize_v_walls(v_walls);
+    // setting all the values of h_walls to 1, indicating that there are horizontal walls
     initialize_h_walls(h_walls);
 
+    // printing the initialized maze
     print_maze(maze);
 
+    // assembling the maze by checking if walls can be broken and breaking them
     assemble_maze(maze, v_walls, h_walls);
 
+    // printing out the maze in perspective of walls
     print_walls(h_walls, v_walls);
     return 0;
 }
